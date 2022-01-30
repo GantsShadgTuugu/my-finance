aba5e7fa50920159c58b42100e2221503bca0f56
tuuguu <2go.ldor@gmail.com>
2022-01-30 23:11:54 +0800

    Clear field iin code iig bichev

--- app1.js
+++ app1.js
@@ -5,7 +5,9 @@ var uiController = (function(){
         inputType: ".add__type",
         inputDescription: ".add__description",
         inputValue: ".add__value",
-        addBtn: ".add__btn"    
+        addBtn: ".add__btn",
+        incomeList: ".income__list",
+        expenseList: ".expenses__list"
     }
 
     return {
@@ -21,14 +23,27 @@ var uiController = (function(){
             return DOMstrings;
         },
 
+        clearField: function(){
+            var fields = document.querySelectorAll(DOMstrings.inputDescription + ", " + DOMstrings.inputValue);
+            // Convert list to arrey
+            var fieldsArr = Array.prototype.slice.call(fields);
+            fieldsArr.foreach(function(el, index, array){
+                el.value = "";
+            })
+            
+            fieldsArr[0].focus();
+            // for(var i = 0; i < fieldsArr.length; i++){
+            //     fieldsArr[i].value = "";
+            // }
+        },
         addList: function(item, type){
             // 1. Орлого зарлагийн алийг нь агуулсан html-ийг бэлтгэнэ
             var html, list;
             if(type === "inc"){
-                list = ".income__list";
+                list = DOMstrings.incomeList;
                 html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
             }else{
-                list = ".expenses__list"
+                list = DOMstrings.expenseList;
                 html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
             // 2. Тэр html дотроо орлого зарлагийн утгуудыг REPLACE ашиглаж өөрчилж өгнө
@@ -98,6 +113,7 @@ var appController = (function(uiController, financeController){
         var item = financeController.addItem(input.type, input.description, input.value);
         // 3. Олж авсан өгөгдлүүдээ веб дээрээ тохирох хэсэгт нь гаргана.
         uiController.addList(item, input.type);
+        uiController.clearField();
         // 4. Төсвийг тооцоолно.
         
         // 5. Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана.
